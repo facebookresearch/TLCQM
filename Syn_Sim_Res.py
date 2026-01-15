5f327a2f1518bc5bae202ec2224eee1606544821
zhangyk8 <yikunzhang@foxmail.com>
2026-01-15 12:15:52 -0800

    Code update

--- Syn_Sim_Res.py
+++ Syn_Sim_Res.py
@@ -11,13 +11,13 @@ import pandas as pd
 
 #=======================================================================================#
 
-# B = 1000
-# res_all = pd.DataFrame()
-# for b in range(1, B+1):
-#     res_tmp = pd.read_csv('./Results/Simulation_Concept_Covariate_'+str(b)+'_new.csv')
-#     res_all = pd.concat([res_all, res_tmp], axis=0)
-# res_all = res_all.reset_index(drop=True)
-# res_all.to_csv('./Results_Syn/Simulation_Concept_Covariate_TLCQM_new.csv', index=False)
+B = 1000
+res_all = pd.DataFrame()
+for b in range(1, B+1):
+    res_tmp = pd.read_csv('./Results/Simulation_Concept_Covariate_'+str(b)+'_new.csv')
+    res_all = pd.concat([res_all, res_tmp], axis=0)
+res_all = res_all.reset_index(drop=True)
+res_all.to_csv('./Results_Syn/Simulation_Concept_Covariate_TLCQM_new.csv', index=False)
 
 B = 1000
 res_all = pd.DataFrame()
@@ -27,19 +27,20 @@ for b in range(1, B+1):
 res_all = res_all.reset_index(drop=True)
 res_all.to_csv('./Results_Syn/Simulation_Concept_Covariate_TLCQM_new2.csv', index=False)    
 
-# res_all = pd.DataFrame()
-# for b in range(1, B+1):
-#     res_tmp = pd.read_csv('./Results/Simulation_Concept_Covariate_'+str(b)+'_Compare.csv')
-#     res_all = pd.concat([res_all, res_tmp], axis=0)
-# res_all = res_all.reset_index(drop=True)
-# res_all.to_csv('./Results_Syn/Simulation_Concept_Covariate_TLCQM_Compare.csv', index=False)
-
+B = 1000
 res_all = pd.DataFrame()
 for b in range(1, B+1):
-    res_tmp = pd.read_csv('./Results/Simulation_Concept_Covariate_'+str(b)+'_Compare_new2.csv')
+    res_tmp = pd.read_csv('./Results/Simulation_Concept_Covariate_'+str(b)+'_Compare.csv')
     res_all = pd.concat([res_all, res_tmp], axis=0)
 res_all = res_all.reset_index(drop=True)
-res_all.to_csv('./Results_Syn/Simulation_Concept_Covariate_TLCQM_Compare_new2.csv', index=False)
+res_all.to_csv('./Results_Syn/Simulation_Concept_Covariate_TLCQM_Compare.csv', index=False)
+
+# res_all = pd.DataFrame()
+# for b in range(1, B+1):
+#     res_tmp = pd.read_csv('./Results/Simulation_Concept_Covariate_'+str(b)+'_Compare_new2.csv')
+#     res_all = pd.concat([res_all, res_tmp], axis=0)
+# res_all = res_all.reset_index(drop=True)
+# res_all.to_csv('./Results_Syn/Simulation_Concept_Covariate_TLCQM_Compare_new2.csv', index=False)
 
 
 B = 500
